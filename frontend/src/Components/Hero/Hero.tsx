b91f5cb3fbfbe529eade1b5f811787b6069642e9
Marco Boutchelski <marco.boutchelski@gmail.com>
2024-05-28 23:08:56 +0100

    feat(hero): add base hero component

--- frontend/src/Components/Hero/Hero.tsx
+++ frontend/src/Components/Hero/Hero.tsx
@@ -1,27 +1,34 @@
 import React from "react";
 import "./Hero.css";
 import { FaArrowDown } from "react-icons/fa";
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 
 interface Props {}
 
 const Hero: React.FC<Props> = () => {
   return (
-    <section className="hero__section">
-      <video
-        className="background-video"
-        src="/resources/videos/Hero-Video.mp4"
-        autoPlay
-        loop
-        muted
-      />
-      <div className="hero__content">
-        <h1 className="hero__title">Find your dream closet using AI</h1>
-        <button className="hero__button btn-primary-outline">
-          Get Started
-          <FaArrowDown className="hero__button-arrow" />
-        </button>
-      </div>
-    </section>
+    <div className="hero__section">
+      <Parallax className="parallax__main" pages={2}>
+        <ParallaxLayer offset={0} factor={1.4}>
+          <video
+            className="background-video"
+            src="/resources/videos/Hero-Video.mp4"
+            autoPlay
+            loop
+            muted
+          />
+        </ParallaxLayer>
+        <ParallaxLayer offset={0} factor={1} sticky={{ start: 0, end: 0.4 }}>
+          <div className="hero__content">
+            <h1 className="hero__title">Find your dream closet using AI</h1>
+            <button className="hero__button btn-primary-outline">
+              Get Started
+              <FaArrowDown className="hero__button-arrow" />
+            </button>
+          </div>
+        </ParallaxLayer>
+      </Parallax>
+    </div>
   );
 };
 
