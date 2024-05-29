300f58c5334db50cfe7ef2e4b4d5e651c757054f
Marco Boutchelski <marco.boutchelski@gmail.com>
2024-05-29 19:26:01 +0100

    feat(hero): shrink background on scroll

--- frontend/src/Components/Hero/Hero.tsx
+++ frontend/src/Components/Hero/Hero.tsx
@@ -1,21 +1,45 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./Hero.css";
 import { FaArrowDown } from "react-icons/fa";
-import { Parallax, ParallaxLayer } from "@react-spring/parallax";
+import { animated, useSpring } from "@react-spring/web";
+import { IParallax, Parallax, ParallaxLayer } from "@react-spring/parallax";
 
 interface Props {}
 
 const Hero: React.FC<Props> = () => {
+  const parallax = useRef<IParallax>(null);
+  const [{ width }, setWidth] = useSpring(() => ({ width: "100%" }));
+
+  const handleScroll = () => {
+    if (parallax.current) {
+      console.log(parallax.current.current);
+      const scrollY = parallax.current.current;
+      const widthValue = Math.max(100 - scrollY * 0.025, 92.5);
+      setWidth({ width: `${widthValue}%` });
+    }
+  };
+
+  useEffect(() => {
+    if (parallax.current) {
+      const container = parallax.current.container.current as HTMLElement;
+      container.addEventListener("scroll", handleScroll);
+      return () => {
+        container.removeEventListener("scroll", handleScroll);
+      };
+    }
+  }, []);
+
   return (
     <div className="hero__section">
-      <Parallax className="parallax__main" pages={2}>
-        <ParallaxLayer offset={0} factor={1.4}>
-          <video
+      <Parallax ref={parallax} className="parallax__main" pages={2}>
+        <ParallaxLayer offset={0} factor={1.6}>
+          <animated.video
             className="background-video"
             src="/resources/videos/Hero-Video.mp4"
             autoPlay
             loop
             muted
+            style={{ width: width.to((w) => w) }}
           />
         </ParallaxLayer>
         <ParallaxLayer offset={0} factor={1} sticky={{ start: 0, end: 0.4 }}>
