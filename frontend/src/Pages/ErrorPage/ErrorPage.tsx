55754e211d124fa5c4b43dae2088048eabda1b96
Marco Boutchelski <marco.boutchelski@gmail.com>
2024-06-01 17:40:08 +0100

    feat(error-page): create error display componenet

--- frontend/src/Pages/ErrorPage/ErrorPage.tsx
+++ frontend/src/Pages/ErrorPage/ErrorPage.tsx
@@ -1,43 +1,19 @@
-import { useRouteError, useNavigate } from "react-router-dom";
+import React from "react";
+import Navbar from "../../Components/Navbar/Navbar.tsx";
+import ErrorDisplay from "../../Components/ErrorDisplay/ErrorDisplay.tsx";
 import "./ErrorPage.css";
 
-interface RouteError {
-  data: string;
-  error: {
-    columnNumber: number;
-    fileName: string;
-    lineNumber: number;
-    message: string;
-    stack: string;
-  };
-  internal: boolean;
-  status: number;
-  statusText: string;
-}
+interface Props {}
 
-export default function ErrorPage() {
-  const navigate = useNavigate();
-  const error = useRouteError() as RouteError;
-
+const ErrorPage: React.FC<Props> = () => {
   return (
-    <div className="error__div">
-      <div className="error__content">
-        <h1 className="error_title">Oops!</h1>
-        <div className="error_message">
-          <p>Sorry, an unexpected error has occurred.</p>
-          <p>
-            <i className="error_message_status">
-              {error.status} {error.statusText || error.error.message}
-            </i>
-          </p>
-        </div>
-        <button
-          className="error_button btn-primary"
-          onClick={() => navigate(-1)}
-        >
-          &larr; Go back
-        </button>
+    <div>
+      <Navbar isScrolled={true} />
+      <div className="error__div">
+        <ErrorDisplay />
       </div>
     </div>
   );
-}
+};
+
+export default ErrorPage;
