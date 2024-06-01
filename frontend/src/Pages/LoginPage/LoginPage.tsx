4b9e083442df5f00177da9632086325a8b4b5617
Marco Boutchelski <marco.boutchelski@gmail.com>
2024-06-01 19:17:14 +0100

    feat(auth): add auth pages

---
+++ frontend/src/Pages/LoginPage/LoginPage.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface Props {}
+
+const LoginPage: React.FC<Props> = () => {
+  return <div>LoginPage</div>;
+};
+
+export default LoginPage;
--- frontend/src/Routes/Routes.tsx
+++ frontend/src/Routes/Routes.tsx
@@ -2,12 +2,18 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App.tsx";
 import HomePage from "../Pages/HomePage/HomePage.tsx";
 import ErrorPage from "../Pages/ErrorPage/ErrorPage.tsx";
+import RegisterPage from "../Pages/RegisterPage/RegisterPage.tsx";
+import LoginPage from "../Pages/LoginPage/LoginPage.tsx";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    children: [{ path: "", element: <HomePage /> }],
+    children: [
+      { path: "", element: <HomePage /> },
+      { path: "login", element: <LoginPage /> },
+      { path: "signup", element: <RegisterPage /> },
+    ],
     errorElement: <ErrorPage />,
   },
 ]);
