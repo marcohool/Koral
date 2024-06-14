9ffb9787cd47cdfe0a5885a9253d14be306da8da
Marco Boutchelski <marco.boutchelski@gmail.com>
2024-06-14 18:40:49 +0100

    feat(gui): add react-toastify

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import { Outlet } from "react-router-dom";
 import { UserProvider } from "./Context/useAuth.tsx";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
     <div className="App">
       <UserProvider>
         <Outlet />
+        <ToastContainer />
       </UserProvider>
     </div>
   );
