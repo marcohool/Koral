469b5d74e3b2eb636f0fb612fc3c544776145596
Marco Boutchelski <marco.boutchelski@gmail.com>
2024-06-29 11:53:36 +0100

    feat(upload-dashboard): add spinner on loading

--- frontend/src/Components/UploadsDashboard/UploadsDashboard.tsx
+++ frontend/src/Components/UploadsDashboard/UploadsDashboard.tsx
@@ -2,6 +2,7 @@ import { FC } from "react";
 import "./UploadsDashboard.css";
 import { Upload } from "../Uploads/UploadsType";
 import UploadCard from "../Uploads/UploadCard/UploadCard.tsx";
+import Spinner from "../Spinner/Spinner.tsx";
 
 interface UploadsDashboardProps {
   uploads?: Upload[];
@@ -13,13 +14,19 @@ const UploadsDashboard: FC<UploadsDashboardProps> = ({ uploads }) => {
       <div className="uploads-dashboard__titles">
         <h3 className="uploads-dashboard__titles-title">All Uploads</h3>
       </div>
-      <div className="uploads-dashboard__grid">
-        {uploads?.map((upload) => (
-          <div key={upload.imageId} className="upload__grid-item">
-            <UploadCard upload={upload} />
-          </div>
-        ))}
-      </div>
+      {uploads ? (
+        <div className="uploads-dashboard__grid">
+          {uploads.map((upload) => (
+            <div key={upload.imageId}>
+              <UploadCard upload={upload} />
+            </div>
+          ))}
+        </div>
+      ) : (
+        <div className="uploads-dashboard__spinner">
+          <Spinner height="60px" />
+        </div>
+      )}
     </div>
   );
 };
