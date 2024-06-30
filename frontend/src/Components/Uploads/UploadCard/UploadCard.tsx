0599e75f9d846f720eff31525ac2c84601500beb
Marco <marco.boutchelski@gmail.com>
2024-06-30 11:59:56 +0100

    feat(upload-card): update upload card styling

--- frontend/src/Components/Uploads/UploadCard/UploadCard.tsx
+++ frontend/src/Components/Uploads/UploadCard/UploadCard.tsx
@@ -1,9 +1,7 @@
 import React from "react";
 import "./resources/styles/UploadCard.css";
 import { StatusType, Upload } from "../UploadsType.ts";
-import { parseDate } from "../helpers.ts";
-import { RiTShirt2Line } from "react-icons/ri";
-import UploadRating from "./UploadRating.tsx";
+import { ParseDate } from "../helpers.ts";
 
 const API_URL = "https://localhost:5001/";
 
@@ -12,40 +10,23 @@ interface Props {
 }
 
 const UploadCard: React.FC<Props> = ({ upload }) => {
-  const status = StatusType[upload.status];
   const error = upload.status === StatusType.Failed ? "error" : undefined;
-  const processing =
-    upload.status === StatusType.Processing ? "processing" : "";
-  const isProcessed = upload.status === StatusType.Processed;
 
   return (
     <div className={`upload__card ${error} `}>
       <div className="upload__card-image">
         <img src={API_URL + upload.imagePath} alt={upload.imageId} />
       </div>
-      <div className={"upload__card__content"}>
+      <div className="upload__card__content">
         <div className="upload__card-titles">
           <h3 className="upload__card-titles-title">
             Placeholder Image Title Value
           </h3>
-          <div className={`upload__card-titles-status`}>
-            {upload.clothingItemsMatched} <RiTShirt2Line />
-          </div>
+          <h3 className="upload__card-titles-subtitle">
+            {ParseDate(upload.createdAt)}
+          </h3>
         </div>
-        <div className="upload__card-details">
-          <p className={`upload__card-details-status ${processing}`}>
-            {isProcessed ? (
-              <>
-                <UploadRating rating={upload.accuracyRating} />{" "}
-              </>
-            ) : (
-              status
-            )}
-          </p>
-          <p className="upload__card-details-date ">
-            {parseDate(upload.createdAt)}
-          </p>
-        </div>
+        <div className="upload__card-details"></div>
       </div>
     </div>
   );
--- frontend/src/Components/Uploads/helpers.ts
+++ frontend/src/Components/Uploads/helpers.ts
@@ -1,3 +1,63 @@
-export const parseDate = (date: string): string => {
-  return new Date(date).toLocaleString();
+export const ParseDate = (date: string): string => {
+  const now = new Date();
+  const providedDate = new Date(date);
+
+  const timeDifference = now.getTime() - providedDate.getTime();
+  const minutesDifference = timeDifference / (1000 * 60);
+
+  if (minutesDifference < 60) {
+    const flooredMinutesDifference = Math.floor(minutesDifference);
+
+    return flooredMinutesDifference == 1
+      ? "1 minute ago"
+      : `${flooredMinutesDifference} minutes ago`;
+  }
+
+  const hourDifference = timeDifference / (1000 * 60 * 60);
+
+  if (hourDifference < 24) {
+    const flooredHourDifference = Math.floor(hourDifference);
+
+    return flooredHourDifference == 1
+      ? "1 hour ago"
+      : `${flooredHourDifference} hours ago`;
+  }
+
+  const dayDifference = hourDifference / 24;
+
+  if (dayDifference < 7) {
+    const flooredDayDifference = Math.floor(dayDifference);
+
+    return flooredDayDifference == 1
+      ? "1 day ago"
+      : `${flooredDayDifference} days ago`;
+  }
+
+  const weekDifference = dayDifference / 7;
+
+  if (weekDifference < 4) {
+    const flooredWeekDifference = Math.floor(weekDifference);
+
+    return flooredWeekDifference == 1
+      ? "1 week ago"
+      : `${flooredWeekDifference} weeks ago`;
+  }
+
+  const monthDifference = weekDifference / 4;
+
+  if (monthDifference < 12) {
+    const flooredMonthDifference = Math.floor(monthDifference);
+
+    return flooredMonthDifference == 1
+      ? "1 month ago"
+      : `${flooredMonthDifference} months ago`;
+  }
+
+  const yearDifference = monthDifference / 12;
+
+  const flooredYearDifference = Math.floor(yearDifference);
+
+  return flooredYearDifference == 1
+    ? "1 year ago"
+    : `${flooredYearDifference} years ago`;
 };
