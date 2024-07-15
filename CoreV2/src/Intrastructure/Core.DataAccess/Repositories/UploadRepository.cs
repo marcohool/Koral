677c3b33ada58b058bccec7a098aacea9d746eee
Marco Boutchelski <marco.boutchelski@gmail.com>
2024-07-15 22:28:47 +0100

    feat(clothing-item): add clothing item repository

--- CoreV2/src/Intrastructure/Core.DataAccess/Repositories/UploadRepository.cs
+++ CoreV2/src/Intrastructure/Core.DataAccess/Repositories/UploadRepository.cs
@@ -1,7 +1,8 @@
 using Core.DataAccess.Persistence;
+using Core.DataAccess.Repositories.Interfaces;
 using Core.Domain.Entities;
 
-namespace Core.DataAccess.Repositories.Interfaces;
+namespace Core.DataAccess.Repositories;
 
 public class UploadRepository(DatabaseContext context)
     : BaseRepository<Upload>(context),
