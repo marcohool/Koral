d62575aa4d3ee3b79a84055b8f67332327de67ac
Marco <marco.boutchelski@gmail.com>
2024-07-28 23:51:23 +0100

    test(image-storage): create base image storage unit test

---
+++ CoreV2/tests/Core.Unit.Test/Services/ImageStorageServiceTests.cs
@@ -0,0 +1,29 @@
+﻿using CloudinaryDotNet;
+using Core.Application.Configuration;
+using Core.Application.Services;
+using Microsoft.Extensions.Options;
+using Moq;
+using CloudinaryConfiguration = Core.Application.Configuration.CloudinaryConfiguration;
+
+namespace Core.UnitTest.Services;
+
+public class ImageStorageServiceTests
+{
+    private readonly Mock<Cloudinary> cloudinaryMock;
+    private Mock<IOptionsMonitor<CloudinaryConfiguration>> mockCloudinaryConfig;
+    private Mock<IOptionsMonitor<ImageOptions>> mockImageOptions;
+
+    private readonly ImageStorageService imageStorageService;
+
+    public ImageStorageServiceTests()
+    {
+        this.mockCloudinaryConfig = new Mock<IOptionsMonitor<CloudinaryConfiguration>>(
+            MockBehavior.Strict
+        );
+        this.mockImageOptions = new Mock<IOptionsMonitor<ImageOptions>>(MockBehavior.Strict);
+        this.cloudinaryMock = new Mock<Cloudinary>(MockBehavior.Strict);
+    }
+
+    [Fact]
+    public async Task UploadImageAsync_ValidImage_ReturnsSecureUrl() { }
+}
