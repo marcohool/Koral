9d6412560c22dc363b64127e8b3a5589b7e84b69
Marco <marco.boutchelski@gmail.com>
2024-08-10 13:11:58 +0100

    test(uploads): return only user uploads

--- CoreV2/tests/Core.Integration.Test/Helpers/DatabaseContextHelper.cs
+++ CoreV2/tests/Core.Integration.Test/Helpers/DatabaseContextHelper.cs
@@ -8,11 +8,15 @@ namespace Core.Integration.Test.Helpers;
 public class DatabaseContextHelper
 {
     private static readonly Guid authenticatedUserId = Guid.NewGuid();
+    private static readonly Guid user2Id = Guid.NewGuid();
 
     public static readonly ApplicationUser authenticatedUser =
         new() { Id = authenticatedUserId.ToString(), Email = "testuser@testemail.com" };
 
-    private static readonly List<ApplicationUser> Users = [authenticatedUser];
+    public static readonly ApplicationUser user2 =
+        new() { Id = user2Id.ToString(), Email = "user2@testemail.com" };
+
+    private static readonly List<ApplicationUser> Users = [authenticatedUser, user2];
 
     private static readonly List<ClothingItem> ClothingItems =
     [
@@ -50,8 +54,72 @@ public class DatabaseContextHelper
             AccuracyRating = 8,
             IsDeleted = false,
             ImageUrl = "https://example-image-hosting.com/old-money-summer-look.jpg",
-            AppUserId = authenticatedUserId.ToString(),
+            AppUserId = authenticatedUser.Id,
             CreatedOn = DateTime.UtcNow,
+            ClothingItems = []
+        },
+        new Upload
+        {
+            Id = Guid.NewGuid(),
+            Title = "Vintage Autumn Vibe",
+            Size = 1024,
+            ContentType = "image/jpeg",
+            Status = UploadStatus.Processing,
+            IsFavourited = false,
+            IsPinned = true,
+            AccuracyRating = 7,
+            IsDeleted = false,
+            ImageUrl = "https://example-image-hosting.com/vintage-autumn-vibe.jpg",
+            AppUserId = authenticatedUser.Id,
+            CreatedOn = DateTime.UtcNow.AddDays(-1),
+            ClothingItems = []
+        },
+        new Upload
+        {
+            Id = Guid.NewGuid(),
+            Title = "Classic Spring Ensemble",
+            Size = 1024,
+            ContentType = "image/png",
+            Status = UploadStatus.Failed,
+            IsFavourited = false,
+            IsPinned = false,
+            AccuracyRating = 9,
+            IsDeleted = true,
+            ImageUrl = "https://example-image-hosting.com/classic-spring-ensemble.png",
+            AppUserId = authenticatedUser.Id,
+            CreatedOn = DateTime.UtcNow.AddDays(-2),
+            ClothingItems = []
+        },
+        new Upload
+        {
+            Id = Guid.NewGuid(),
+            Title = "Winter Elegance Series",
+            Size = 1024,
+            ContentType = "image/jpeg",
+            Status = UploadStatus.Processed,
+            IsFavourited = true,
+            IsPinned = true,
+            AccuracyRating = 5,
+            IsDeleted = false,
+            ImageUrl = "https://example-image-hosting.com/winter-elegance-series.jpg",
+            AppUserId = authenticatedUser.Id,
+            CreatedOn = DateTime.UtcNow.AddDays(-3),
+            ClothingItems = []
+        },
+        new Upload
+        {
+            Id = Guid.NewGuid(),
+            Title = "Beach Day Outfits",
+            Size = 1024,
+            ContentType = "image/jpeg",
+            Status = UploadStatus.Processed,
+            IsFavourited = true,
+            IsPinned = false,
+            AccuracyRating = 8,
+            IsDeleted = false,
+            ImageUrl = "https://example-image-hosting.com/beach-day-outfits.jpg",
+            AppUserId = user2.Id,
+            CreatedOn = DateTime.UtcNow.AddDays(-4),
             ClothingItems = []
         }
     ];
--- CoreV2/tests/Core.Integration.Test/Tests/UploadTests.cs
+++ CoreV2/tests/Core.Integration.Test/Tests/UploadTests.cs
@@ -1,14 +1,32 @@
-﻿namespace Core.Integration.Test.Tests;
+﻿using Core.Application.Dtos.Upload;
+using Core.Integration.Test.Helpers;
+using FluentAssertions;
+using Newtonsoft.Json;
+
+namespace Core.Integration.Test.Tests;
 
 public class UploadTests(CustomWebApplicationFactory factory) : BaseIntegrationTest(factory)
 {
     [Fact]
-    public async Task GetAllAsync_ReturnsAllUploads()
+    public async Task GetAllAsync_ReturnsAllUserUploads()
     {
         HttpClient client = this.HttpClient;
 
         HttpResponseMessage response = await client.GetAsync("/uploads");
 
         response.EnsureSuccessStatusCode();
+
+        List<UploadResponseDto>? uploads = JsonConvert.DeserializeObject<List<UploadResponseDto>>(
+            await response.Content.ReadAsStringAsync()
+        );
+
+        uploads
+            .Should()
+            .HaveCount(
+                this.DbContext.Uploads.Where(u =>
+                        u.AppUserId == DatabaseContextHelper.authenticatedUser.Id
+                    )
+                    .Count()
+            );
     }
 }
