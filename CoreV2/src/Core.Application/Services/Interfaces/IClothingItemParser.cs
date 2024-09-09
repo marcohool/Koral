82ed5f1dcdf32d4253b8dc00f2ec44f53f00c10d
Marco Boutchelski <marco.boutchelski@gmail.com>
2024-09-09 08:05:18 +0100

    feat(clothing-item): add base parser service

---
+++ CoreV2/src/Core.Application/Services/Interfaces/IClothingItemParser.cs
@@ -0,0 +1,9 @@
+﻿using Core.Domain.Entities;
+using Microsoft.AspNetCore.Http;
+
+namespace Core.Application.Services.Interfaces;
+
+public interface IClothingItemParser
+{
+    Task<List<ClothingItem>> Parse(IFormFile file);
+}
---
+++ CoreV2/src/Core.Application/Services/JsonClothingItemParser.cs
@@ -0,0 +1,13 @@
+﻿using Core.Application.Services.Interfaces;
+using Core.Domain.Entities;
+using Microsoft.AspNetCore.Http;
+
+namespace Core.Application.Services;
+
+public class JsonClothingItemParser : IClothingItemParser
+{
+    public Task<List<ClothingItem>> Parse(IFormFile file)
+    {
+        throw new NotImplementedException();
+    }
+}
