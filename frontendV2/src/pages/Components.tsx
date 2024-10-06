fbfc28504715edeec5080daf40adb9032defc248
Marco Boutchelski <marco.boutchelski@gmail.com>
2024-10-06 01:38:50 +0100

    feat(button): configure button component

--- frontendV2/src/pages/Components.tsx
+++ frontendV2/src/pages/Components.tsx
@@ -3,9 +3,13 @@ import Button from "components/button";
 
 const Components: FC = () => {
   return (
-    <div>
-      Components
+    <div className="px-4 py-4 flex justify-center items-center flex-col gap-10">
+      <h1 className="text-5xl">Components</h1>
+      <div className="flex flex-col gap-4">
       <Button size="lg" variant="default" onClick={() => console.log("test")}>Click Here</Button>
+      <Button size="lg" variant="secondary" onClick={() => console.log("test")}>Click Here</Button>
+      <Button size="lg" variant="outline" onClick={() => console.log("test")}>Click Here</Button>
+      </div>
     </div>
   );
 };
--- frontendV2/tailwind.config.js
+++ frontendV2/tailwind.config.js
@@ -24,7 +24,8 @@ export default {
   			},
   			primary: {
   				DEFAULT: 'hsl(var(--primary))',
-  				foreground: 'hsl(var(--primary-foreground))'
+  				foreground: 'hsl(var(--primary-foreground))',
+					dark: ('hsl(var(--primary-dark))')
   			},
   			secondary: {
   				DEFAULT: 'hsl(var(--secondary))',
