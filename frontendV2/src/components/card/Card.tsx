bfe7bcd49ccc98ec0c4948a3d03c797538df6a04
Marco <marco.boutchelski@gmail.com>
2024-11-24 18:20:10 +0000

    feat(card): implement scroll on drag

--- frontendV2/src/components/card/Card.tsx
+++ frontendV2/src/components/card/Card.tsx
@@ -1,6 +1,7 @@
-import { FC, ReactNode, useState } from 'react';
+import { FC, ReactNode, useRef, useState } from 'react';
 import { cn } from 'utils/utils';
 import { ScrollArea } from 'components/scrollArea';
+import useScrollOnDrag from 'react-scroll-ondrag';
 
 interface CardProps {
   imageUrl: string;
@@ -15,11 +16,16 @@ const CardImage: FC<{ children: ReactNode; className?: string }> = ({
   children,
   className,
 }) => {
+  const containerRef = useRef(null);
+  const { events } = useScrollOnDrag(containerRef);
+
   return (
-    <div className={cn('overflow-hidden', className)}>
-      <ScrollArea className="h-full" scrollHideDelay={0}>
-        {children}
-      </ScrollArea>
+    <div
+      className={cn('overflow-hidden', className)}
+      {...events}
+      ref={containerRef}
+    >
+      {children}
     </div>
   );
 };
@@ -52,17 +58,16 @@ const Card: FC<CardProps> = ({
       onMouseLeave={() => setIsHovered(false)}
       onTouchStart={() => setIsHovered(true)}
       onTouchEnd={() => setIsHovered(false)}
-      onClick={onClick}
     >
-      <CardImage className="flex-grow">
+      <CardImage className="flex-grow cursor-grab">
         <img
           src={imageUrl}
           alt="Card image"
-          className="object-cover w-full h-full"
+          className="object-cover w-full h-full pointer-events-none"
           style={{ minHeight: isHovered ? '130%' : '100%' }}
         />
       </CardImage>
-      {isHovered ? hoveredBody : visibleBody}
+      <div onClick={onClick}>{isHovered ? hoveredBody : visibleBody}</div>
     </div>
   );
 };
